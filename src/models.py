0b910fa7f15f9d4b2e30e0afc402a607c143131c
Cone Perez <oscarerik@gmail.com>
2024-09-23 23:46:29 -0600

    corregir path models para no usar src

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 # models.py
 import enum
-from src.base import Base
+from base import Base
 from sqlalchemy import Column, String, Enum, ForeignKey, Float, BigInteger, Integer, TIMESTAMP, Double, Boolean
 from sqlalchemy.orm import relationship, backref
 
